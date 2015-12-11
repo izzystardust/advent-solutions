107c85e6f7a13fe9407c20248c54a830c742ae8e
Robert Heitzmann <Robert.Heitzmann@me.com>
2015-12-12 00:04:03 +0100

    strict variant cleaned branch

--- 11/11.pl
+++ 11/11.pl
@@ -1,17 +1,17 @@
 sub shitty_regex {
     $str = shift
-    return $str =~ /.*(abc|bcd|cde|def|efg|fgh|ghi|hik|ijk|jkl|klm|lmn|mno|nop|opq|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz).*/;
+    return $str =~ /(abc|bcd|cde|def|efg|fgh|ghi|hik|ijk|jkl|klm|lmn|mno|nop|opq|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz)/;
 }
 
 sub valid {
-    $str=shift;
-    return ($str =~ /(.)\1.*(.)\2/ and $str =~ /^((?![oil]).)*$/ and shitty_regex($str));
+    $str = shift;
+    return ($str =~ /(.)\1.*(.)\2/ and $str =~ /^[^oil]+$/ and shitty_regex($str));
 }
 
 $pw = ++$ARGV[0];
 
-until ( valid( $pw ) ) {
-        $pw = ++$pw;
+until ( valid($pw) ) {
+    $pw = ++$pw;
 }
 
 print "$pw\n"
---
+++ 11/README.md
@@ -0,0 +1,10 @@
+# AOC day 11
+   
+## Facts
+Perl 5 can do string arithmetics with "digits" 'a' to 'z'!
+
+## Files
+* 11.pl - slight variant of concise day 11 solution.
+    Two regexes were shortened.
+* 11_strict_varint.pl - same, but using `strict` etc.
+    Checks for 8 lowercase letters a-z (stated condition ad-hoc true).
