2a825231f43eabf785440481145a52bb6fbe7414
Isabelle Miller <izzy@stardust.fm>
2015-12-11 11:16:10 -0500

    Discovered fold, couldn't help it

--- 10/10.sh
+++ 10/10.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 iter=$1
-for i in `seq 1 $2`; do iter=`echo $iter | sed 's/./& /g' | tr ' ' "\n" | sed '/^$/d' | uniq -c | tr  -d ' \n'`; done
+for i in `seq 1 $2`; do iter=`echo $iter | fold -w 1 | uniq -c | tr  -d ' \n'`; done
 echo $iter | tr -d '\n' | wc -c
